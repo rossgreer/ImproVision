a5dd36088b412271e18afc639364c749a4bab103
Laura Fleig <100258636+Greencouch82@users.noreply.github.com>
2024-02-07 15:58:59 -0800

    rough draft of cues

---
+++ improv_harmony.py
@@ -0,0 +1,24 @@
+# setup: 4 musicians, in a semi-circle around camera, nobody directly in the middle
+
+# default camera position: horizontally level, facing middle of semi-circle
+
+
+# CUES: 
+
+# first, cue in individually - musicians should take notice of communication and remember it until final cue
+# for each musician you want to change, from left to right:
+#   make 'eye contact'
+#   wait 1 second
+#   small nod up (i.e., quickly tilt 30? 45? degrees upward, then return to default state): move up 1 half step
+#   big nod up (i.e., quickly tilt 60? 90? degrees upward, then return to default state): move up 1 whole step
+#   small nod down: move down 1 half step
+#   big nod down: move down 1 whole step
+#   if you want more than 1 whole step:
+#       pause 0.5 seconds while continuing to look at musician
+#       then nod again
+#   then turn to the next musician you want to change
+
+# once all individual cues have taken place:
+#   pan around
+#   return to default state (facing the middle)
+#   then big nod (90 degrees upward, then return to default state) to cue simultaneous changes
